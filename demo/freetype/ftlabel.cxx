598a37f2abcb918c12f2184f743c4b5178fc30a0
Helbrass <Helbrass@gmail.com>
2011-06-23 11:49:10 +0300

    comments to qt freetype2 demo

--- demo/freetype/ftlabel.cxx
+++ demo/freetype/ftlabel.cxx
@@ -20,11 +20,13 @@ void ftlabel::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
 
     QString text = "The quick brown fox jumps over the lazy dog";
-    //QString text = "l";
+    // coordinates where we will start painting our text string:
     int x = 30;
     int y = 30;
+    // reusable freetype error variable:
     int error = 0;
 
+    // one letter at a time:
     for (int i = 0; i < text.size(); i++) {
 
         FT_UInt glyph_index = FT_Get_Char_Index( m_face, text[i].toAscii() );
@@ -33,12 +35,15 @@ void ftlabel::paintEvent(QPaintEvent *event) {
 
 #if(LCD_RENDERING)
         {
-            error = FT_Load_Glyph( m_face, glyph_index, FT_LOAD_TARGET_LCD);
+            // DejaVu font is visually similar with autohinter and without, but Consolas is crippled with autohinter,
+            // so we will load without autohinting:
+            error = FT_Load_Glyph( m_face, glyph_index, FT_LOAD_TARGET_LCD | FT_LOAD_NO_AUTOHINT);
             if (error) {
                 qDebug() << "FT_Load_Glyph error: " << error;
                 return;
             }
 
+            // in LCD mode additional filtering is required:
             error = FT_Library_SetLcdFilter(m_library, FT_LCD_FILTER_DEFAULT);
             if (error) {
                 qDebug() << "FT_Library_SetLcdFilter error: " << error;
@@ -51,15 +56,35 @@ void ftlabel::paintEvent(QPaintEvent *event) {
                 return;
             }
 
+            // height and width of bitmap:
             int _h = m_face->glyph->bitmap.rows;
+            // in LCD mode bitmap is 3 times wider, because of R, G and B channels,
+            // dividing /3 to get target image width:
             int _w = m_face->glyph->bitmap.width/3;
+
+            // bytes per bitmap row:
             int _p = m_face->glyph->bitmap.pitch;
 
-            uchar *src = m_face->glyph->bitmap.buffer;
+            // I have absolutely no idea what's the difference between ARGB32 and ARGB32_Premultiplied:
             glyphImage = new QImage(_w, _h, QImage::Format_ARGB32_Premultiplied);
+            // filling image will absolutely transparent black:
             glyphImage->fill(qRgba(0, 0, 0, 0xff));
 
-            int bit = 0; // address in buffer
+            /*
+              NOTE: this is not optimized version of painting. It was simplified for demo purposes.
+                    1) Optimized version should create full image buffer in memory and just pass it
+                       to QImage constructor.
+                    2) Pointer to bitmap buffer may be incremented instead of keeping separate "bit"
+                       positioning integer.
+                    3) Difference between m_face->glyph->bitmap.width and m_face->glyph->bitmap.pitch
+                       is constant, it can be calculated once and used as increment to src pointer after
+                       each row is processed.
+                    4) Color value calculation can be skipped and background color value used in case when
+                       bitmap value is 0.
+             */
+            uchar *src = m_face->glyph->bitmap.buffer;
+            // position inside src[] buffer:
+            int bit = 0;
             for (int y = 0; y < _h; y++) {
 
                 for (int x = 0; x < _w; x++) {
@@ -68,6 +93,21 @@ void ftlabel::paintEvent(QPaintEvent *event) {
                     uchar g = src[bit++];
                     uchar b = src[bit++];
 
+                    /*
+                      formula for every color goes like this:
+                      color = background + ( ( foreground - background ) * source / 255 )
+                      where:
+                      color        final value of color for image.
+                      background   color of panel or surface for text painting.
+                      foreground   color of text.
+                      source       transparency level from freetype2 rendered bitmap. It's
+                                   value divided by 255 to simulate transparency double.
+
+                      In our current case background is constant 0xff, foreground is constant 0,
+                      so we can hardcode required values for this demo.
+
+                      We have to perform this operation for all 3 colors we are getting from bitmap.
+                     */
                     r = 0xff + ( -0xff * r / 255 );
                     g = 0xff + ( -0xff * g / 255 );
                     b = 0xff + ( -0xff * b / 255 );
@@ -76,12 +116,13 @@ void ftlabel::paintEvent(QPaintEvent *event) {
 
                 }
 
-                bit = _p * (y + 1); // go to next line, start from 0
+                // move bitmap pointer to next line:
+                bit = _p * (y + 1);
             }
         }
 #else
         {
-            error = FT_Load_Glyph( m_face, glyph_index, FT_LOAD_DEFAULT );
+            error = FT_Load_Glyph( m_face, glyph_index, FT_LOAD_DEFAULT);
             if (error) {
                 qDebug() << "FT_Load_Glyph error: " << error;
                 continue;
@@ -93,12 +134,14 @@ void ftlabel::paintEvent(QPaintEvent *event) {
                 continue;
             }
 
-            glyphImage = new QImage(m_face->glyph->bitmap.buffer,
-                              m_face->glyph->bitmap.width, // 27-7
-                              m_face->glyph->bitmap.rows, // 9-9
-                              m_face->glyph->bitmap.pitch, // 28-7
-                              QImage::Format_Indexed8);
+            // direct values from freetype2 may be used in case of monochrome rendering:
+            glyphImage = new QImage(m_face->glyph->bitmap.buffer, // uchar buffer of bits
+                              m_face->glyph->bitmap.width, // width of image
+                              m_face->glyph->bitmap.rows, // height of image
+                              m_face->glyph->bitmap.pitch, // bytes per line
+                              QImage::Format_Indexed8); // image format
 
+            // feature of QImage, setting foreground to black for any level of transparency:
             QVector<QRgb> colorTable;
             for (int i = 0; i < 256; ++i) {
                 colorTable << qRgba(0, 0, 0, i);
@@ -107,8 +150,18 @@ void ftlabel::paintEvent(QPaintEvent *event) {
         }
 #endif
 
+        /*
+          Fonts and glyphs as I udnerstood them:
+          - There is a font and there is a glyph. Font represents full font family, has it's size, it's
+            imaginary borders. Glyph is image of concrete character from font. There is a padding
+            between glyph boundary box and font borders. That padding is number of pixels from "current
+            pen position" to glyph boundary bor. We can get its value from m_face->glyph->bitmap_left.
+          - Step from one font character to next is "advance". In freetype2 tutorials it's used with
+            shifting: m_face->glyph->advance.x >> 6.
+          - We will draw image on coordinates X + padding, then we will increment X with font advance value,
+            and it will be prepared to draw next image.
+         */
         painter.drawImage(x + m_face->glyph->bitmap_left, y - m_face->glyph->bitmap_top, *glyphImage);
-
         x += m_face->glyph->advance.x >> 6;
 
     }
@@ -121,13 +174,18 @@ ftlabel::ftlabel(QWidget *parent) : QWidget(parent) {
         return;
     }
     error = FT_New_Face( m_library, "/usr/share/fonts/truetype/DejaVuSansMono.ttf", 0, &m_face);
+    //error = FT_New_Face( m_library, "/usr/local/share/fonts/c/CONSOLA.ttf", 0, &m_face);
     if (error) {
         qDebug() << "FT_New_Face error: " << error;
         return;
     }
+
+    // physical DPI calculated by Qt itself, it's using X.org API to get physical screen dimension and resolution:
     int dpyx = physicalDpiX();
     int dpyy = physicalDpiY();
     qDebug() << "Physical dpy: " << dpyx << " x " << dpyy;
+
+    // freetype2 is using 1/64 of point for char size, so 10 points will be 10*64:
     error = FT_Set_Char_Size( m_face, 0, 10*64, dpyx, dpyy);
     if (error) {
         qDebug() << "FT_Set_Char_Size error: " << error;
