3c990e16a0558943c14a3d24f5d57d51ad9bdc62
Alexander Pospishniy <pospishniy@gmail.com>
2011-06-25 18:27:21 +0300

    Changes to java font demo

--- demo/java/src/swinglabel/Swinglabel.java
+++ demo/java/src/swinglabel/Swinglabel.java
@@ -2,8 +2,9 @@ package swinglabel;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GridLayout;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,25 +16,38 @@ public class Swinglabel implements Runnable {
         System.setProperty("swing.boldMetal", "false");
         SwingUtilities.invokeLater(new Swinglabel());
     }
-    
+
     private JPanel createContentPane() {
-        JPanel panel = new JPanel(new GridLayout(9, 1));
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBackground(Color.WHITE);
         panel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
-        
-        panel.add(createLabel("DejaVu Sans Mono", 10));
-        panel.add(createLabel("Consolas", 10));
-        panel.add(new JLabel("<html>&nbsp;"));
-        panel.add(createLabel("DejaVu Sans Mono", 12));
-        panel.add(createLabel("Consolas", 12));
-        panel.add(new JLabel("<html>&nbsp;"));
-        panel.add(createLabel(Font.MONOSPACED, 0));
-        panel.add(createLabel(Font.SANS_SERIF, 0));
-        panel.add(createLabel(Font.SERIF, 0));
-        
+
+        for (int a = 6; a <= 18; a += 2) {
+            panel.add(createLabel("DejaVu Sans Mono", a));
+        }
+
+        panel.add(Box.createVerticalStrut(20));
+
+        for (int a = 18; a >= 6; a -= 2) {
+            panel.add(createLabel("Consolas", a));
+        }
+
+        panel.add(Box.createVerticalStrut(20));
+
+        for (int a = 6; a <= 18; a += 2) {
+            panel.add(createLabel(Font.MONOSPACED, a));
+        }
+
+        panel.add(Box.createVerticalStrut(20));
+
+        for (int a = 18; a >= 6; a -= 2) {
+            panel.add(createLabel(Font.SANS_SERIF, a));
+        }
+
         return panel;
     }
-    
+
     private JLabel createLabel(String fontFamily, int fontSize) {
         JLabel label = new JLabel("The quick brown fox jumps over the lazy dog (" + fontFamily + (fontSize == 0 ? ")" : " " + fontSize + ")"));
         label.setForeground(Color.BLACK);
@@ -44,7 +58,7 @@ public class Swinglabel implements Runnable {
 
     @Override
     public void run() {
-        JFrame frame = new JFrame("swinglabel");
+        JFrame frame = new JFrame("java-font-demo");
         frame.setContentPane(createContentPane());
         frame.setBackground(Color.WHITE);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
